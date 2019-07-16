35b79311e01938cc5c76faacda2a2a5c6c797154
pipeza379 <kittapon.j@ku.th>
2019-07-17 01:44:46 +0700

    carousel

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Carousel from './library/carousel';
+import Carousel from './library/Carousel';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<Carousel />, document.getElementById('root'));
